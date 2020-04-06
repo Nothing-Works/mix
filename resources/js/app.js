e970dba1f31fb1ff6e81d7647bfb2d8c2f174b03
AndySong <andylauszp@gmail.com>
2020-04-06 16:55:23 +1200

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import Vue from 'vue'
+import vuetify from './plugin'
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+new Vue({
+    vuetify
+}).$mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-alert type="success">
+      I'm a success alert.
+    </v-alert>
+
+    <v-alert type="info">
+      I'm an info alert.
+    </v-alert>
+
+    <v-alert type="warning">
+      I'm a warning alert.
+    </v-alert>
+
+    <v-alert type="error">
+      I'm an error alert.
+    </v-alert>
+  </v-app>
+</template>
